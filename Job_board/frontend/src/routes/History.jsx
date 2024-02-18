3ddf39736805512e63123db0ba9ff64020e41d11
Oussa-Err <dexterlecter0@gmail.com>
2024-02-18 21:36:05 +0100

    advanced personal info page

--- Job_board/frontend/src/routes/History.jsx
+++ Job_board/frontend/src/routes/History.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const History = () => {
   return (
-    <div>History</div>
-  )
-}
+      <div className="p-4 sm:ml-64 pt-28 h-[80dvh]">
+        <h1 className="mb-2 text-5xl md:p-4 font-bold tracking-tight">
+          Applied Jobs
+        </h1>
+        <div className="flex flex-col md:flex-row gap-7">
+          <div
+            href="#"
+            className="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Jobs submitted:
+            </h5>
+            <div></div>
+          </div>
+        </div>
+      </div>
+  );
+};
 
-export default History
+export default History;
--- Job_board/frontend/src/routes/Information.jsx
+++ Job_board/frontend/src/routes/Information.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Information = () => {
   return (
-    <div>Information</div>
-  )
-}
+    <div>
+      <div className="p-4 sm:ml-64 pt-28 h-[80dvh]">
+        <h1 className="mb-2 text-5xl md:p-4 font-bold tracking-tight">
+          Personal info
+        </h1>
+        <div
+          href="#"
+          className="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+          <div className="flex flex-col gap-7">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              First Name:
+            </h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Last Name:
+            </h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Email:
+            </h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              status:
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Information
+export default Information;
