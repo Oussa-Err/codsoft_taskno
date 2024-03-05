8c788ce189568276cd1494421c57c81a4c806dd6
Oussa-Err <dexterlecter0@gmail.com>
2024-03-05 16:29:00 +0100

    outlet navigation to login page

--- Job_board/frontend/src/components/UserRoute.jsx
+++ Job_board/frontend/src/components/UserRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const UserRoute = ({ children }) => {
   const loggedInUser = JSON.parse(localStorage.getItem("userInfo"));
-  return loggedInUser ? children : <Navigate to="/" />;
+  return loggedInUser ? children : <Navigate to="/login" />;
 };
 
 export default UserRoute;
