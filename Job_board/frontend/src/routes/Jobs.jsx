8a32484503d9d545e6ff551a54ea4717e7e8639f
Oussa-Err <dexterlecter0@gmail.com>
2024-05-02 03:01:53 +0100

    navigation bug fixed for job detail page

--- Job_board/frontend/src/routes/Jobs.jsx
+++ Job_board/frontend/src/routes/Jobs.jsx
@@ -137,7 +137,7 @@ const Jobs = () => {
                 </div>
               </div>
               <a
-                href={`job/${job.id}`}
+                href={`/job/${job.id}`}
                 className="inline-flex w-fit items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Read more
