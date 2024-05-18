cfa91cdb4583d4f2c54ce1d5ccadfce5f58bc7c7
Oussa-Err <dexterlecter0@gmail.com>
2024-05-18 01:11:42 +0100

    configured backend uri

--- Job_board/frontend/src/redux/actions/jobAction.jsx
+++ Job_board/frontend/src/redux/actions/jobAction.jsx
@@ -16,8 +16,8 @@ axios.interceptors.request.use(config => {
     return config;
 });
 
-axios.defaults.baseURL = `http://127.0.0.1:8080/api/v1`
-// axios.defaults.baseURL = `${import.meta.env.VITE_BACKEND_URI}/api/v1`
+// axios.defaults.baseURL = `http://127.0.0.1:8080/api/v1`
+axios.defaults.baseURL = `${import.meta.env.VITE_BACKEND_URI}/api/v1`
 
 export const jobsAction = (pageNumber, keyword = '') => async (dispatch) => {
     try {
