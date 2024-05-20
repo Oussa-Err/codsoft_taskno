f8c27bf00308ac76dae33f3aa139cb31912cbb41
Oussa-Err <dexterlecter0@gmail.com>
2024-05-20 17:05:07 +0100

    moved morgan & nodemon to devDep

--- Online_quiz_maker/backend/functions/api.js
+++ Online_quiz_maker/backend/functions/api.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const router = express.Router()
-const cors = require('cors')
-const morgan = require('morgan')
 const controller = require("./controller/controllers")
 const globalErrorHandler = require("./controller/globalErrorHandler")
+const morgan = require('morgan')
+const cors = require('cors')
 const CustomErr = require("./utils/CustomErrClass")
 const serverless = require('serverless-http')
+
 const dotenv = require('dotenv')
 dotenv.config({ path: "./.env" })
 
@@ -33,12 +34,6 @@ app.get('/', (req, res) => {
 router.get("/quizzes", controller.getQuizzes)
 router.get("/quiz/:id", controller.isAuth, controller.getQuiz)
 router.post("/create", controller.createQuiz)
-
-
-// app.all('*', (req, res, next) => {
-//     const err = new CustomErr(`can't find ${req.originalUrl} on the server`, 404)
-//     next(err)
-// })
 
 app.use(globalErrorHandler)
 
