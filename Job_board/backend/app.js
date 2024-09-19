87d6ed15cf23d3dacfb8a84ce56b5eb2f500424b
Oussa-Err <dexterlecter0@gmail.com>
2024-09-19 20:21:14 +0100

    npm i

--- Job_board/backend/app.js
+++ Job_board/backend/app.js
@@ -55,7 +55,7 @@ app.use(bodyParser.urlencoded({
 const rateLimiter = rateLimit({
     max: 3,
     windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	limit: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes).
     message: "we have received too many request from this ip, try after one hour"
 })
 
