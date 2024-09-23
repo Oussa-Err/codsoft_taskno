dbd1e2b800c592770754a7c440bac7df375e6b70
Oussa-Err <dexterlecter0@gmail.com>
2024-09-23 13:41:06 +0100

    morgan not found onrender bug removed 3

--- Job_board/backend/app.js
+++ Job_board/backend/app.js
@@ -41,7 +41,7 @@ app.use(sanitize());
 app.use(xss())
 
 // Logging middleware
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
